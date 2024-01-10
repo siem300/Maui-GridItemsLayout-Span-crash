c2164717c6fe094ea0137d6c65b57a81047bf056
siem300 <svdkluft@gmail.com>
2024-01-10 10:04:42 +0100

    initial checkin

---
+++ MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public int ColumnCount { get; set; } = 3;
+        private void UpdateColumnCount(object sender, EventArgs e)
+        {
+            ColumnCount++;
+            Dispatcher.Dispatch(() =>
+            {
+                OnPropertyChanged("ColumnCount");
+            });
+        }
+    }
+
+}
